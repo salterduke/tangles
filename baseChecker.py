312ab39e3751febf850a9b07ca86c9d7f154f83f
salterduke <saltermich1@msor.vuw.ac.nz>
2022-07-18 12:47:28 +1200

    Modified test type, working on checkers.

--- baseChecker.py
+++ baseChecker.py
@@ -106,35 +106,3 @@ class commChecker():
         return(aveCommSim / self.aveSim)
         # todo also add per comm values
 
-
-    def getOverlapMI(self, foundcover):
-        # todo: discuss whether this is a meaningful measure, as tangles are *not* overlapping
-        # todo also make this fucker work too.
-        overlapMeta = self.getOverlapMetadata()
-        overlapMeta = self.getOverlapMetadata()
-        self.graphData = self.graphData.join(overlapMeta, on="node")
-
-        #### get rid of the NANS.
-        self.graphData.dropna(inplace = True)
-
-        ### ? Why?
-        self.graphData['GOcount'] = pd.to_numeric(self.graphData['GOcount'])
-
-        #### fucking no idea here on number of bins.
-        # https://stats.stackexchange.com/questions/179674/number-of-bins-when-computing-mutual-information
-        bins = math.floor(math.sqrt(len(self.graphData)/5))
-        ## from https://stackoverflow.com/questions/20491028/optimal-way-to-compute-pairwise-mutual-information-using-numpy
-        contingencyTable = np.histogram2d(self.graphData["commCount"], self.graphData["GOcount"], bins=bins)[0]
-        mi = skl.mutual_info_score(None, None, contingency=contingencyTable)
-        print("MI: {}".format(mi))
-
-        ax = self.graphData.plot.scatter(x='GOcount',y='commCount')
-        ax.set_xlabel("Num of GO annotations")
-        ax.set_ylabel("Community membership count")
-        plt.title("Comm count vs GO count: {}, E: {}, MI: {}"\
-            .format(self.job['outName'], commQual, mi))
-        plt.savefig('{}/{}-ScatterPlotCommVsGO.pdf'.\
-            format(self.job['outputFolder'], self.job['outName']),
-            bbox_inches='tight')
-        plt.close()
-        ##### working out NMI (Lancichinetti 2009)
