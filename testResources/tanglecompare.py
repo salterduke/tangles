ff3b14e185e0c828c575710ff6c69d24f1ad4fbe
salterduke <saltermich1@msor.vuw.ac.nz>
2022-07-21 13:29:26 +1200

    Tangle compare appears to be working so far.

--- testResources/tanglecompare.py
+++ testResources/tanglecompare.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 import collections
 import sys
+import string
 
 # this is the main function
 def compareSeps(filename1, filename2):
@@ -62,153 +64,144 @@ def compareCovers(filename1, filename2):
 
 
 
-# # todo compareTangles is not tested!
-# def compareTangles(filename1, filename2):
-#     seps1 = pd.read_csv(filename1, sep="\t")
-#     seps2 = pd.read_csv(filename2, sep="\t")
-#
-#
-#     groundset = extractVertices(seps1["side1"][0]) | extractVertices(seps1["side2"][0])
-#
-#     small1 = seps1.loc[seps1["orientation"] == 1]
-#     small2 = seps1.loc[seps1["orientation"] == 2]
-#     small = list(small1["side1"]) + list(small2["side2"])
-#
-#     # missedSeps = seps2.loc[~seps2["side1"].isin(set(seps1["side1"]))]
-#     missedSeps = set(seps2["side1"]) - set(seps1["side1"])
-#     missedSeps = [extractVertices(side) for side in missedSeps]
-#     print("Missed seps before: {}".format(len(missedSeps)))
-#
-#     print("len old, {} len new {}".format(len(seps2), len(seps1)))
-#
-#     additionalSmall = []
-#
-#     ### list of sets
-#     smallAsSets = [extractVertices(side) for side in small]
-#     for i in range(len(smallAsSets)):
-#         for j in range(i + 1, len(smallAsSets)):
-#             newSep = smallAsSets[i] | smallAsSets[j]
-#             if newSep in missedSeps:
-#                 missedSeps.remove(newSep)
-#             elif groundset - newSep in missedSeps:
-#                 print("Removing complement 1")
-#                 missedSeps.remove(newSep)
-#
-#             additionalSmall.append(newSep)
-#     print("Missed seps after: {}".format(len(missedSeps)))
-#
-#     for i in range(len(additionalSmall)):
-#         for j in range(i + 1, len(additionalSmall)):
-#             newSep = additionalSmall[i] | additionalSmall[j]
-#             if newSep in missedSeps:
-#                 missedSeps.remove(newSep)
-#             elif groundset - newSep in missedSeps:
-#                 print("Removing complement 2")
-#                 missedSeps.remove(newSep)
-#     print("Missed seps after stage 2: {}".format(len(missedSeps)))
-#     # print("\n".join([str(sep) for sep in missedSeps]))
-#
-#     copyMissedSeps = missedSeps.copy()
-#     # print("\n".join([str(sep) for sep in copyMissedSeps]))
-#     print("---------------------")
-#     testSet = smallAsSets + additionalSmall
-#
-#     for sep in copyMissedSeps:
-#         # print("testing: {}".format(sep))
-#         for smallSep in testSet:
-#             if sep.issubset(smallSep):
-#                 # print(sep)
-#                 print("Found: {}".format(sep))
-#                 missedSeps.remove(sep)
-#                 break
-#
-#     print("Missed seps after stage 3: {}".format(len(missedSeps)))
-#     # print("\n".join([str(sep) for sep in missedSeps]))
-#     printMissed(seps2, missedSeps)
-#
-#
-# def extractVertices(textStr):
-#     plain = textStr.replace("[", "").replace("]", "")
-#     plainSet = set(plain.split(", "))
-#     plainSet = {item.replace("'", "") for item in plainSet}
-#     return plainSet
-#
-# # todo extractEdges
-# def extractEdges(textStr):
-#     plain = textStr.replace("[", "").replace("]", "")
-#     plainSet = set(plain.split(", "))
-#     plainSet = {item.replace("'", "") for item in plainSet}
-#     return plainSet
-#
-#
-#
-# def printMissed(fullData, seps):
-#     for sep in seps:
-#         sepStr = "['" + "', '".join(sorted(sep)) + "']"
-#         # print(sepStr)
-#         ord = int(fullData.loc[fullData["side1"] == sepStr]["order"])
-#         print("{}: {}".format(ord, sepStr))
-#
-# def compare(filename1, filename2):
-#     tset1 = pd.read_csv(filename1)
-#     columns1 = list(tset1)
-#
-#     tset2 = pd.read_csv(filename2)
-#     columns2 = list(tset2)
-#
-#     if columns1 == columns2:
-#         print("So far so good, headers are equal")
-#
-#     dict1 = collections.defaultdict(list)
-#
-#     print(columns1)
-#     #### skip first col - just indices
-#     for col in columns1[1:]:
-#         newset = set()
-#         order = col.split("=")[1]
-#         tangle = tset1[col]
-#         for item in tangle:
-#             if str(item).find("1.00") > -1:
-#                 newset.add(item)
-#         dict1[order].append(newset)
-#         if len(newset) == 0:
-#             print("Huh, no 1s")
-#             print(tangle)
-#
-#
-#
-#     for col in columns2[1:]:
-#         order = col.split("=")[1]
-#
-#         testset = set()
-#         tangle = tset2[col]
-#         for item in tangle:
-#             if str(item).find("1.00") > -1:
-#                 testset.add(item)
-#
-#         if len(testset) == 0:
-#             print("Huh, no 1s")
-#             print(tangle)
-#
-#
-#         if testset in dict1[order]:
-#             print("Yay, found {} from file2 in file1".format(col))
-#             # print(testset)
-#         else:
-#             print("Bummer, did not find {} from file2 in file1".format(col))
-#             print(testset)
+# this will have to be a little kludgy:
+# a) don't think can guarantee all v's will be given explicitly in orientation file
+# so pass in number of v's and make sure to use orientation file with ids, not names
+# b) massive pain in the arse to take into account v merging in pre-processing
+# so must ensure that both files have the same pre-processing, which means that:
+# c) make sure the number of v's passed is the number *after* pre-processing
+
+class comparerClass():
+
+    def compareTangles(self, filename1, filename2, N):
+
+        orients = [0,0]
+        orients[0] = pd.read_csv(filename1, sep=",")
+        orients[1] = pd.read_csv(filename2, sep=",")
+
+
+        self.groundset = set(range(N))
+
+        # first check column headers
+        if not all(orients[0].columns == orients[1].columns):
+            print("Column headers don't match")
+            print(pd.DataFrame({"file1": orients[0].columns, "file2": orients[1].columns}) )
+            print("Testing columns that do match")
+            mismatch = np.where(orients[0].columns != orients[1].columns)[0][0]
+        else:
+            mismatch = len(orients[0].columns) + 1
+
+        # start at col 1 as 0 is index
+        tangResults = []
+        for id in range(1,mismatch-1):
+            cols = [0,0]
+            for fid in range(2):
+                cols[fid] = orients[fid].iloc[:,id]
+                cols[fid] = cols[fid][cols[fid].notna()]
+
+            cols = sorted(cols, key=len)
+            shortCol = list(map(self.getSmallSide, cols[0]))
+            longCol = list(map(self.getSmallSide, cols[1]))
+            colResults = np.zeros(len(longCol), dtype=bool)
+            for sid, sideFromLong in enumerate(longCol):
+                if sideFromLong in shortCol:
+                    colResults[sid] = True
+                else:
+                    for sideFromShort in shortCol:
+                        if sideFromLong.issubset(sideFromShort):
+                            colResults[sid] = True
+                            break
+            tangResults.append(all(colResults))
+            # colDF = pd.DataFrame({"seps": longCol, "result": colResults})
+            # print(colDF)
+            # print(shortCol)
+            dummy = 1
+
+        resultsDF = pd.DataFrame({"tangle": orients[0].columns[1:mismatch-1], "result": tangResults})
+        print(resultsDF)
+
+    def getSmallSide(self, sideStr):
+
+        left, right = map(str.strip, sideStr.split("/"))
+        if "G\\" in left:
+            complement = set(map(int, right.strip("[").strip("]").split(",")))
+            smallSet = self.groundset - complement
+        else:
+            smallSet = set(map(int, left.strip("[").strip("]").split(",")))
+        return(smallSet)
+
+
+# todo extractEdges
+def extractEdges(textStr):
+    plain = textStr.replace("[", "").replace("]", "")
+    plainSet = set(plain.split(", "))
+    plainSet = {item.replace("'", "") for item in plainSet}
+    return plainSet
 
 
 
+def printMissed(fullData, seps):
+    for sep in seps:
+        sepStr = "['" + "', '".join(sorted(sep)) + "']"
+        # print(sepStr)
+        ord = int(fullData.loc[fullData["side1"] == sepStr]["order"])
+        print("{}: {}".format(ord, sepStr))
 
-# compare("YeastGSComp4-AvoidProps-Old.csv", "YeastGSComp4-AvoidProps-New.csv")
-# compare("YeastGSComp4-AvoidProps-New.csv", "YeastGSComp4-AvoidProps-Old.csv")
+def compare(filename1, filename2):
+    tset1 = pd.read_csv(filename1)
+    columns1 = list(tset1)
+
+    tset2 = pd.read_csv(filename2)
+    columns2 = list(tset2)
+
+    if columns1 == columns2:
+        print("So far so good, headers are equal")
+
+    dict1 = collections.defaultdict(list)
+
+    print(columns1)
+    #### skip first col - just indices
+    for col in columns1[1:]:
+        newset = set()
+        order = col.split("=")[1]
+        tangle = tset1[col]
+        for item in tangle:
+            if str(item).find("1.00") > -1:
+                newset.add(item)
+        dict1[order].append(newset)
+        if len(newset) == 0:
+            print("Huh, no 1s")
+            print(tangle)
+
+
+
+    for col in columns2[1:]:
+        order = col.split("=")[1]
+
+        testset = set()
+        tangle = tset2[col]
+        for item in tangle:
+            if str(item).find("1.00") > -1:
+                testset.add(item)
+
+        if len(testset) == 0:
+            print("Huh, no 1s")
+            print(tangle)
+
+
+        if testset in dict1[order]:
+            print("Yay, found {} from file2 in file1".format(col))
+            # print(testset)
+        else:
+            print("Bummer, did not find {} from file2 in file1".format(col))
+            print(testset)
+
 
 
 print("---------------------------------------------")
-# compareSeps("../outputCutfinder/TinyEdges-SepList-CF.tsv", "../outputGHU_all/TinyEdges-SepList-GHU.tsv")
-compareSeps("../outputCutfinder/YeastGSCompB-SepList-CF.tsv", "../outputGHU_all/YeastGSCompB-SepList-GHU.tsv")
-# compareSeps("../outputCutfinder/YeastGSCompA-SepList-CF.tsv", "../outputGHU_all/YeastGSCompA-SepList-GHU.tsv")
+# compareSeps("../outputCutfinder/YeastGSCompB-SepList-CF.tsv", "../outputGHU_all/YeastGSCompB-SepList-GHU.tsv")
+comparer = comparerClass()
+comparer.compareTangles("./testResources/TinyEdges-Orientations-Original.csv", "./testResources/TinyEdges-Orientations-Supersets.csv", 9)
 print("---------------------------------------------")
 
 
